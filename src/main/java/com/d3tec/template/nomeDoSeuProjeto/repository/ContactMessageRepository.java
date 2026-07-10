5ab2ca1941c0ee339b1b9083e3b6e7cfc1c0db6d
Guilhermehenrique8 <guilherme.pereira.dantas39@gmail.com>
2026-07-09 22:20:11 -0300

    feat: adiciona ContactMessageRepository

--- src/main/java/com/d3tec/template/nomeDoSeuProjeto/repository/ContactMessageRepository.java
+++ src/main/java/com/d3tec/template/nomeDoSeuProjeto/repository/ContactMessageRepository.java
@@ -1,4 +1,9 @@
 package com.d3tec.template.nomeDoSeuProjeto.repository;
 
-public interface ContactMessageRepository {
+import com.d3tec.template.nomeDoSeuProjeto.entity.ContactMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactMessageRepository extends JpaRepository<ContactMessage, Long> {
 }
